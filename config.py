6bdd17deac3d97cbe093cc1aefae44b87b4ff0de
tmy2 <41803129+tmy2@users.noreply.github.com>
2025-07-28 22:35:48 -0400

    Update configuration values for MQTT, device address, and battery capacity

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #MQTT Server Info
-MQTT_HOST='IP'
-MQTT_USER='user'
-MQTT_PASS='pass'
+MQTT_HOST='10.0.0.120:8123'
+MQTT_USER='ttt'
+MQTT_PASS='pass0900'
 # Mac Address of the Juntec BT
-JUNTEC_ADDR="XX:XX:XX:XX:XX:XX"
+JUNTEC_ADDR="38:3B:26:5C:2D:0B"
 # Battery Bank Capacity in Ah
-BATT_CAP=200
+BATT_CAP=50
 # Port where the RS485 is located
 RS485 = '/dev/ttyUSB0'
