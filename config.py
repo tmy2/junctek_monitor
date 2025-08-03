360f6a03f5e1d4fc9aa5d7aee2a7c72d920106e1
tmy2 <41803129+tmy2@users.noreply.github.com>
2025-08-03 18:07:33 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #MQTT Server Info
-MQTT_HOST=10.0.0.120:8123
-MQTT_USER=user
-MQTT_PASS=pass
+MQTT_HOST=10.0.0.122:8123
+MQTT_USER=admin
+MQTT_PASS=admin
 # Mac Address of the Juntec BT
-JUNTEC_ADDR=38:3B:26:5C:2D:0B
+JUNTEC_ADDR=38:3b:26:5c:2d:0b
 # Battery Bank Capacity in Ah
 BATT_CAP=50
 # Port where the RS485 is located
